dded4610bdbb2a37e66d3102c2ee2a46473d7b1a
Sebastian Ibanez <Sibanez0081@conestogac.on.ca>
2022-03-17 19:35:35 -0400

    Added inHand and scoreValue attribute to the CARD ADT.

--- Blackjack/Source.c
+++ Blackjack/Source.c
@@ -5,6 +5,12 @@ int main(void) {
 	printf("Hello world!\n");
 
 	CARD* deck = initialiseDeck();
+
+	printf("\nThe original deck:\n\n");
 	printDeck(deck);
+	shuffleDeck(deck);
+	printf("\nThe shuffled deck:\n\n");
+	printDeck(deck);
+
 	return 0;
 }
