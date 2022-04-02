e1a0e9ecc353c89e9dd4d5379961935cc1e47fe0
Deborah Ann Punnoose George <Dgeorge3159@conestogac.on.ca>
2022-04-02 16:00:21 -0400

    added input checking to the hit/stand option

--- Blackjack/Gameplay.c
+++ Blackjack/Gameplay.c
@@ -67,19 +67,27 @@ void gameplay(PPLAYER thePlayer) {
 				printf("HIT or STAND?\na: HIT\nb: STAND\n");			//THIS NEEDS TO BE UPDATED TO MATCH INTERFACE
 
 				//get players choice
+				bool input = false;
 				fseek(stdin, 0, SEEK_END);
 				char pchoice = (char)getc(stdin);										//THIS NEEDS INPUT CHECKING
 				fseek(stdin, 0, SEEK_END);
 				//check players choice
-				switch (pchoice) {										//DOES THIS NEED A DEFAULT CASE??? MAYBE FOR INPUT CHECKING
+				do {
+					switch (pchoice) {										//DOES THIS NEED A DEFAULT CASE??? MAYBE FOR INPUT CHECKING
 
-				case 'a':		//if hit, deal a card to the player
-					hit(thePlayer, dealCard(theDeck));
-					break;
-				case 'b':		//if stand, break this loop
-					pturn = false;
-					break;
-				}
+					case 'a':		//if hit, deal a card to the player
+						system("cls");
+						hit(thePlayer, dealCard(theDeck));
+						break;
+					case 'b':		//if stand, break this loop
+						pturn = false;
+						system("cls");
+						break;
+					default:
+						printf("Invalid input, please try again. \n\n");
+						break;
+					}
+				} while (input == false);
 
 			pscore = getHandTotal(getPlayerHand(thePlayer));
 
@@ -91,7 +99,7 @@ void gameplay(PPLAYER thePlayer) {
 			}
 
 			//clear screen
-			system("cls");		//clears screen before ending game
+			//system("cls");		//clears screen before ending game
 
 		} while (pturn);
 
@@ -133,17 +141,17 @@ void gameplay(PPLAYER thePlayer) {
 			//printf("Player bust!\nDealer wins\n");
 			PrintLoser();
 			printf("You BUST!\n\n");
-			//print player's final cards
-			printf("Your final hand is:\n");
-			printf("\n");
-			printPlayerHandFinal(getPlayerHand(thePlayer));
-			PrintTotalCards(getHandTotal(getPlayerHand(thePlayer)));
-			printf("------------------------------------------\n\n");
 			//print dealer's final cards
 			printf("The dealer's final hand is:\n");
 			printf("\n");
 			printDealerHandFinal(getPlayerHand(theDealer));
 			PrintDealerTotalCards(getHandTotal(getPlayerHand(theDealer)));
+			printf("------------------------------------------\n\n");
+			//print player's final cards
+			printf("Your final hand is:\n");
+			printf("\n");
+			printPlayerHandFinal(getPlayerHand(thePlayer));
+			PrintTotalCards(getHandTotal(getPlayerHand(thePlayer)));
 
 			//decrease player score
 			decreaseScore(thePlayer);
@@ -152,17 +160,17 @@ void gameplay(PPLAYER thePlayer) {
 			//printf("Dealer bust!\nPlayer wins\n");
 			PrintWinner();
 			printf("The Dealer BUST!\n\n");
-			//print player's final cards
-			printf("Your final hand is:\n");
-			printf("\n");
-			printPlayerHandFinal(getPlayerHand(thePlayer));
-			PrintTotalCards(getHandTotal(getPlayerHand(thePlayer)));
-			printf("------------------------------------------\n\n");
 			//print dealer's final cards
 			printf("The dealer's final hand is:\n");
 			printf("\n");
 			printDealerHandFinal(getPlayerHand(theDealer));
 			PrintDealerTotalCards(getHandTotal(getPlayerHand(theDealer)));
+			printf("------------------------------------------\n\n");
+			//print player's final cards
+			printf("Your final hand is:\n");
+			printf("\n");
+			printPlayerHandFinal(getPlayerHand(thePlayer));
+			PrintTotalCards(getHandTotal(getPlayerHand(thePlayer)));
 
 			//increase player score
 			increaseScore(thePlayer);
@@ -174,17 +182,17 @@ void gameplay(PPLAYER thePlayer) {
 		else if (getHandTotal(getPlayerHand(thePlayer)) > getHandTotal(getPlayerHand(theDealer))) { 
 			//printf("Player wins\n");
 			PrintWinner();
-			//print player's final cards
-			printf("Your final hand is:\n");
-			printf("\n");
-			printPlayerHandFinal(getPlayerHand(thePlayer));
-			PrintTotalCards(getHandTotal(getPlayerHand(thePlayer)));
-			printf("------------------------------------------\n\n");
 			//print dealer's final cards
 			printf("The dealer's final hand is:\n");
 			printf("\n");
 			printDealerHandFinal(getPlayerHand(theDealer));
 			PrintDealerTotalCards(getHandTotal(getPlayerHand(theDealer)));
+			printf("------------------------------------------\n\n");
+			//print player's final cards
+			printf("Your final hand is:\n");
+			printf("\n");
+			printPlayerHandFinal(getPlayerHand(thePlayer));
+			PrintTotalCards(getHandTotal(getPlayerHand(thePlayer)));
 
 			//handle score
 			increaseScore(thePlayer);
@@ -192,17 +200,17 @@ void gameplay(PPLAYER thePlayer) {
 		else if (getHandTotal(getPlayerHand(thePlayer)) < getHandTotal(getPlayerHand(theDealer))) {
 			//printf("Dealer wins\n");
 			PrintLoser();
-			//print player's final cards
-			printf("Your final hand is:\n");
-			printf("\n");
-			printPlayerHandFinal(getPlayerHand(thePlayer));
-			PrintTotalCards(getHandTotal(getPlayerHand(thePlayer)));
-			printf("------------------------------------------\n\n");
 			//print dealer's final cards
 			printf("The dealer's final hand is:\n");
 			printf("\n");
 			printDealerHandFinal(getPlayerHand(theDealer));
 			PrintDealerTotalCards(getHandTotal(getPlayerHand(theDealer)));
+			printf("------------------------------------------\n\n");
+			//print player's final cards
+			printf("Your final hand is:\n");
+			printf("\n");
+			printPlayerHandFinal(getPlayerHand(thePlayer));
+			PrintTotalCards(getHandTotal(getPlayerHand(thePlayer)));
 
 			//handle score
 			decreaseScore(thePlayer);
